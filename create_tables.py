f8a19f16d8a0b5c0c8cf23b727aae64c31909f56
Rostislav Rabiec <rosta.rabiec@icloud.com>
2023-08-17 19:36:47 +0200

    Process of table 'doctor' completed.

--- create_tables.py
+++ create_tables.py
@@ -10,11 +10,28 @@ table_address = '''CREATE TABLE address (
                    zip_code VARCHAR(6)
                 )'''
 
+doctor_table = '''CREATE TABLE doctor (
+                  id SERIAL,
+                  name VARCHAR(255),
+                  surname VARCHAR(255),
+                  address_id INT,
+                  phone_number VARCHAR(20),
+                  email VARCHAR(255)
+               )'''
+
 # Define SQL commands for inserting sample data into the tables
 insert_address_data = '''INSERT INTO address (street, street_number, city, zip_code) VALUES'''
 
+insert_doctor_data = '''INSERT INTO doctor (name, surname, address_id, phone_number, email) VALUES'''
+
 # Define sample data for the tables
-values_address = [('Doktorská', 1, 'Engetov', '123 00'), ('Pacientská', 1, 'Engetov', '111 11'), ('Pacientská', 2, 'Engetov', '567 89')]
+values_address = [
+    ('Doktorská', 1, 'Engetov', '123 00'), 
+    ('Pacientská', 1, 'Engetov', '111 11'), 
+    ('Pacientská', 2, 'Engetov', '567 89')
+    ]
+
+values_doctor = [('Jan', 'Doktor', 1, '+420 123 456 789', 'doktor@engeto.cz')]
 
 # Connect to the database and create the tables
 mydb = mysql.connector.connect(
@@ -28,9 +45,15 @@ mycursor = mydb.cursor()
 # Create the address table
 mycursor.execute(table_address)
 
+# Create the doctor table
+mycursor.execute(doctor_table)
+
 # Insert sample data into the address table
 for value in values_address:
     mycursor.execute(insert_address_data, value)
+
+# Insert sample data into the doctor table
+mycursor.execute(insert_doctor_data, values_doctor)
 
 # Commit the changes
 mydb.commit()
