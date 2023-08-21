3d798dba14cec5c385e5095f2d96e62c925d99a6
Rostislav Rabiec <rosta.rabiec@icloud.com>
2023-08-21 15:48:22 +0200

    Process of table 'prescription' completed.

--- create_tables.py
+++ create_tables.py
@@ -33,7 +33,16 @@ table_medicament = '''CREATE TABLE medicament (
                       price_insurance FLOAT,
                       price_patient FLOAT,
                       unit VARCHAR(10)
-                      )'''
+                   )'''
+
+table_prescription = '''CREATE TABLE prescription (
+                        id SERIAL,
+                        doctor_id INT,
+                        patient_id INT,
+                        valid_from DATETIME,
+                        valid_to DATETIME,
+                        is_released BOOLEAN
+                     )'''
 
 # Define SQL commands for inserting sample data into the tables
 insert_address_data = '''INSERT INTO address (street, street_number, city, zip_code) VALUES'''
@@ -42,7 +51,9 @@ insert_doctor_data = '''INSERT INTO doctor (name, surname, address_id, phone_num
 
 insert_patient_data = '''INSERT INTO patient (name, surname, address_id, insurance_company) VALUES'''
 
-insert_data_medicament = '''INSERT INTO medicament (name, price_insurance, price_patient, unit) VALUES'''
+insert_medicament_data = '''INSERT INTO medicament (name, price_insurance, price_patient, unit) VALUES'''
+
+insert_prescription_data = '''INSERT INTO prescription (doctor_id, patient_id, valid_from, valid_to, is_released) VALUES'''
 
 # Define sample data for the tables
 values_address = [
@@ -65,6 +76,15 @@ values_medicament = [
     ('Common Pills', 5, 6.1, 'package'),
     ('Super Sirup', 12, 8, 'mililiter'),
     ('Extra Sirup', 16.3, 10.3, 'mililiter')
+]
+
+values_prescription = [
+    (1, 1, '2019-10-06 11:35:12', '2019-10-16 11:35:12', True),
+    (1, 2, '2019-10-06 12:00:06', '2019-10-16 12:00:06', False),
+    (1, 3, '2019-10-06 14:04:53', '2019-10-16 14:04:53', True),
+    (1, 1, '2019-10-08 08:05:40', '2019-10-18 08:05:40', True),
+    (1, 1, '2019-11-11 09:12:42', '2019-11-21 09:12:42', False),
+    (1, 2, '2019-11-11 10:07:35', '2019-11-21 10:07:35', False)
 ]
 
 # Connect to the database and create the tables
@@ -101,7 +121,11 @@ for value in values_patient:
 
 # Insert sample data into medicament table
 for value in values_medicament:
-    mycursor.execute(insert_data_medicament, value)
+    mycursor.execute(insert_medicament_data, value)
+
+# Insert sample data into prescription table
+for value in values_prescription:
+    mycursor.execute(insert_prescription_data, value)
 
 # Commit the changes
 mydb.commit()
